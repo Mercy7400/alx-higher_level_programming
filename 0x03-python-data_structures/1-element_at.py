ae322852b58d020f9a85fd1dc0027465a847e9f8
Mercy7400 <nyokabikamu100@gmail.com>
2024-01-08 15:01:01 -0800

    second commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,10 +5,9 @@ def element_at(my_list, idx):
         return None
     return my_list[idx]
 
-# Example usage:
+#usage:
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     idx = 3
     result = element_at(my_list, idx)
     print("Element at index {} is {}".format(idx, result))
-
