1838cfdd3ff3e9d2e92c5a12a7fe305f41f7cf6a
Mercy7400 <nyokabikamu100@gmail.com>
2024-01-08 14:44:32 -0800

    first commit

---
+++ 0x03-python-data_structures/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from lists import print_list_integer
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
+
+if __name__ == "__main__":
+    main()
+
